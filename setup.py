d8334bdf44b1186061c9e79dd00ce9828acaf844
Marina Pavlova <pavlova.marina.v@gmail.com>
2021-03-22 18:06:11 +0300

    version changed to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='redash_tools',
-      version='0.0.15',
+      version='1.0.0',
       description='Tools to backup, batch update, template redash queries and dashboards',
       url='http://github.com/pavlova-marina/redash_tools',
       author='Marina Pavlova',
