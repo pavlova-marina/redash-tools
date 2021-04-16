37e520f6f4e4900301190cac2b8b74b90cc607e6
Marina Pavlova <pavlova.marina.v@gmail.com>
2021-04-16 22:05:40 +0300

    license etc upload

--- setup.py
+++ setup.py
@@ -8,11 +8,11 @@ setup(name='redash_tools',
       author='Marina Pavlova',
       author_email='pavlova.marina.v@gmail.com',
       url='http://github.com/pavlova-marina/redash-tools',
-      download_url='https://github.com/pavlova-marina/redash-tools/archive/v1.0.2.tar.gz',
+      download_url='https://github.com/pavlova-marina/redash-tools/archive/refs/tags/v1.0.2.tar.gz',
       keywords=['redash'],
       install_requires=['requests'],
       python_requires='>=3.6',
-      classifiers = [
+      classifiers=[
                   'Development Status :: 3 - Alpha',
                   'Intended Audience :: Developers',
                   'Topic :: Software Development :: Build Tools',
