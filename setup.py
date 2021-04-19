46912acb3d7bb996ec6d06a665ed2d4d3c65b685
Marina Pavlova <pavlova.marina.v@gmail.com>
2021-04-19 11:38:03 +0300

    fix

--- setup.py
+++ setup.py
@@ -1,26 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='redash_tools',
-      packages=['redash_tools'],
-      version='1.0.2',
+      packages=find_packages(),
+      version='1.0.3',
       license='MIT',
       description='Tools to backup, batch update, template redash queries and dashboards',
       author='Marina Pavlova',
       author_email='pavlova.marina.v@gmail.com',
       url='http://github.com/pavlova-marina/redash-tools',
-      download_url='https://github.com/pavlova-marina/redash-tools/archive/refs/tags/v1.0.2.tar.gz',
+      download_url='https://github.com/pavlova-marina/redash-tools/archive/refs/tags/v1.0.3.tar.gz',
       keywords=['redash'],
       install_requires=['requests'],
       python_requires='>=3.6',
       classifiers=[
-                  'Development Status :: 3 - Alpha',
-                  'Intended Audience :: Developers',
-                  'Topic :: Software Development :: Build Tools',
                   'License :: OSI Approved :: MIT License',
                   'Operating System :: OS Independent',
-                  'Programming Language :: Python :: 3',
-                  'Programming Language :: Python :: 3.4',
-                  'Programming Language :: Python :: 3.5',
                   'Programming Language :: Python :: 3.6',
               ]
       )
